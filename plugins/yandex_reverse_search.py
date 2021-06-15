66bada6f73cf10ede4b74d0cfb10f2d7ce1eda1c
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-15 09:30:39 +0000

    auto: black code format

--- plugins/yandex_reverse_search.py
+++ plugins/yandex_reverse_search.py
@@ -3,21 +3,24 @@
 # All rights reserved.
 
 import os
-import string
 import random
+import string
 from typing import Dict
 
 import requests
-
-from userge import userge, Config, Message, pool
+from userge import Config, Message, pool, userge
 from userge.utils import progress
 
 
-@userge.on_cmd("yrs", about={
-    'header': "Yandex Reverse Search",
-    'description': "Reverse Search any Image/sticker",
-    'usage': "{tr}yrs [Reply to image | sticker]",
-    'note': "Gif & Animated Stickers won't work!"})
+@userge.on_cmd(
+    "yrs",
+    about={
+        "header": "Yandex Reverse Search",
+        "description": "Reverse Search any Image/sticker",
+        "usage": "{tr}yrs [Reply to image | sticker]",
+        "note": "Gif & Animated Stickers won't work!",
+    },
+)
 async def labstack(message: Message):
 
     await message.edit("Found Yandex Result. Lemme pass some Soup!")
@@ -31,7 +34,7 @@ async def labstack(message: Message):
             message=message.reply_to_message,
             file_name=Config.DOWN_PATH,
             progress=progress,
-            progress_args=(message, "Found Yandex Result. Lemme pass some Soup!")
+            progress_args=(message, "Found Yandex Result. Lemme pass some Soup!"),
         )
     else:
         return await message.err("Media not found!")
@@ -40,26 +43,27 @@ async def labstack(message: Message):
     filename = os.path.basename(dl_loc)
 
     file = {"name": filename, "type": "", "size": int(filesize)}
-    user_id = ''.join(random.choice(
-        string.ascii_lowercase + string.ascii_uppercase + string.digits
-    ) for _ in range(16))
+    user_id = "".join(
+        random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits)
+        for _ in range(16)
+    )
     data = {"ttl": 604800, "files": [file]}
     headers = {
-        'up-user-id': user_id,
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/83.0.4103.97 Safari/537.36'
+        "up-user-id": user_id,
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64)"
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/83.0.4103.97 Safari/537.36",
     }
-    r = (await _post(
-        url="https://up.labstack.com/api/v1/links",
-        json=data,
-        headers=headers
-    )).json()
+    r = (
+        await _post(
+            url="https://up.labstack.com/api/v1/links", json=data, headers=headers
+        )
+    ).json()
 
     files = {
-        'files': (filename, open(dl_loc, 'rb')),
+        "files": (filename, open(dl_loc, "rb")),
     }
-    send_url = "https://up.labstack.com/api/v1/links/{}/send".format(r['code'])
+    send_url = "https://up.labstack.com/api/v1/links/{}/send".format(r["code"])
     response = await _post(url=send_url, headers=headers, files=files)
     if (response.status_code) == 200:
         link = (
@@ -73,15 +77,12 @@ async def labstack(message: Message):
 
 @pool.run_in_thread
 def _post(
-    url: str,
-    headers: dict,
-    json: Dict[str, str] = None,
-    files: Dict[str, str] = None
+    url: str, headers: dict, json: Dict[str, str] = None, files: Dict[str, str] = None
 ):
-    args = {'url': url, 'headers': headers}
+    args = {"url": url, "headers": headers}
     if files:
-        args['files'] = files
+        args["files"] = files
     elif json:
-        args['json'] = json
+        args["json"] = json
 
     return requests.post(**args)
